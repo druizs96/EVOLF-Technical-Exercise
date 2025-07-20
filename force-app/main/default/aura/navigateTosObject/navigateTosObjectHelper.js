56953ed0d02f40192a8f694cedbbd18c7f0b2b00
David Ruiz <druizsoto96@gmail.com>
2025-07-20 22:20:03 +0200

    Componente para mostrar toast de error y navegar al registro creado

---
+++ force-app/main/default/aura/navigateTosObject/navigateTosObjectHelper.js
@@ -0,0 +1,11 @@
+({
+    showToast : function(component, title, message, type) {
+    var toastEvent = $A.get("e.force:showToast");
+    toastEvent.setParams({
+        "title": title,
+        "message": message,
+        "type": type  // 'success', 'error', 'warning', 'info'
+    });
+    toastEvent.fire();
+}
+})
